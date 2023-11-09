c5f5f6a396ba82f463347d1aba0781db83aedf57
sugarete <cthien3602@gmail.com>
2023-11-09 15:25:45 +0700

    Venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Documents\Code\Project\ytbdl\.venv
+set VIRTUAL_ENV=d:\Code\PBL3\YoutubeDownloaderWeb\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
