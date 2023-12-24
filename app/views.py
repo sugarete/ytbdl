c84b11a71b40d5473d359ab79f708df1f053186d
sugarete <cthien3602@gmail.com>
2023-12-24 21:47:50 +0700

    Noel commit

--- app/views.py
+++ app/views.py
@@ -1,18 +1,22 @@
 from flask import Blueprint, render_template, request
 from pytube import YouTube
-from flask import Flask, request, render_template, send_file, jsonify, redirect, url_for
+from flask import Flask, request, render_template, send_file
+from urllib.parse import quote
 import os
 from datetime import timedelta
 
 views = Blueprint('views', __name__)
 
-# download_directory = 'downloaded_videos'
-# if not os.path.exists(download_directory):
-#     os.makedirs(download_directory)
+download_directory = 'downloaded_videos'
+if not os.path.exists(download_directory):
+    os.makedirs(download_directory)
+
+def format_time(seconds):
+    return str(timedelta(seconds=seconds))
 
 @views.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 # @views.route('/login')
 # def registry():
@@ -29,34 +33,24 @@ def home():
 #         video_url = YouTube(url).streams.get_by_itag(22)
 #         video = video_url.download(download_directory)
 #         return send_file(video, as_attachment=True)
-    
-# @app.route('/extract', methods=['POST', 'GET']) 
-# def extract():
-#     if request.method == 'POST':
-#         url = request.form['url'x``]
-#         video = YouTube(url)
-#         video_info = {
-#             'title': video.title,
-#             'length': video.length,
-#         }
-#         return jsonify(video_info)
-
-def format_time(seconds):
-    return str(timedelta(seconds=seconds))
 
 @views.route('/extract', methods=['POST', 'GET'])
 def extract():
     if request.method == 'POST':
         url = request.form['url']
         video = YouTube(url)
-
-        unique_resolutions = list(dict.fromkeys([i.resolution for i in video.streams if i.resolution]))
-        resolution_values = [int(resolution.split("p")[0]) for resolution in unique_resolutions]
-        resolution_values.sort()
-
-        return render_template('vidinfo.html', video=video, resolutions = resolution_values, format_time=format_time)
+        return render_template('vidinfo.html', video=video, url = url, format_time=format_time)
     
-# @views.route('/extract/<url>')
-# def extract_url(url):
-#     video = YouTube(url)
-#     return redirect(f"{video.streams.get_highest_resolution().url}",)
+@views.route('/download', methods=['GET'])
+def download():
+    url = request.args.get('url')
+    quality = request.args.get('quality')
+    
+    if url and quality:
+        video = YouTube(url)
+        video_url = video.streams.get_by_itag(quality)
+        video_path = video_url.download(download_directory)
+        return send_file(video_path, as_attachment=True)
+    else:
+        # Handle invalid request (missing parameters)
+        return "Invalid request"
--- test/ytb_extractor.py
+++ test/ytb_extractor.py
@@ -1,6 +1,12 @@
+import requests
+import validators
 from pytube import YouTube
 
 url = input("Enter the url: ")
+
+if not validators.url(url):
+    print("Invalid url")
+    exit()
 
 my_video = YouTube(url)
 print(my_video.title)
@@ -8,9 +14,17 @@ print(my_video.length)
 print(my_video.thumbnail_url)
 resolution =[int(i.split("p")[0]) for i in (list(dict.fromkeys([i.resolution for i in my_video.streams if i.resolution])))]
 resolution.sort()
-print(resolution)
-# for stream in my_video.streams:
-#     print(stream.resolution)
 
-print(my_video.streams.get_by_itag(22).url);
-    
+streams = set()
+for i in my_video.streams.filter(mime_type="video/mp4", progressive=True):
+    streams.add(i.itag)
+
+for itag in my_video.streams:
+    print(itag.itag, itag.resolution, itag.mime_type, itag.abr, itag.url)
+
+print("Video streams----------------------")
+for streams in my_video.streams.filter(mime_type="video/mp4", progressive=True):
+    print(streams)
+
+print("Video streams")
+print(streams)
