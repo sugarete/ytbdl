7fc8b204e784775636762529a19c9125e003c5be
sugarete <cthien3602@gmail.com>
2023-12-26 22:37:00 +0700

    Add audio only option MP4 format - Convert to MP3: Working On

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, send_file
 from pytube import YouTube
-from flask import Flask, request, render_template, send_file
+import ffmpeg
 from urllib.parse import quote
 import os
 from datetime import timedelta
@@ -10,6 +10,10 @@ views = Blueprint('views', __name__)
 download_directory = 'downloaded_videos'
 if not os.path.exists(download_directory):
     os.makedirs(download_directory)
+
+send_directory = 'send_videos'
+if not os.path.exists(send_directory):
+    os.makedirs(send_directory)
 
 def format_time(seconds):
     return str(timedelta(seconds=seconds))
@@ -25,14 +29,6 @@ def home():
 # @views.route('/signup')
 # def signup():
 #     return render_template('signup.html')
-            
-# @views.route('/dl', methods=['POST'])
-# def download():
-#     if request.method == 'POST':
-#         url = request.form['url']
-#         video_url = YouTube(url).streams.get_by_itag(22)
-#         video = video_url.download(download_directory)
-#         return send_file(video, as_attachment=True)
 
 @views.route('/extract', methods=['POST', 'GET'])
 def extract():
@@ -41,16 +37,45 @@ def extract():
         video = YouTube(url)
         return render_template('vidinfo.html', video=video, url = url, format_time=format_time)
     
-@views.route('/download', methods=['GET'])
-def download():
+@views.route('/download_video', methods=['GET'])
+def download_video():
     url = request.args.get('url')
     quality = request.args.get('quality')
     
     if url and quality:
         video = YouTube(url)
         video_url = video.streams.get_by_itag(quality)
-        video_path = video_url.download(download_directory)
+        video_path = video_url.download(send_directory)
         return send_file(video_path, as_attachment=True)
     else:
-        # Handle invalid request (missing parameters)
         return "Invalid request"
+
+def convert_to_mp3(audio_path):
+    out_audio_path = audio_path[:-4] + '.mp3'
+    print("Input: " + audio_path)
+    print("Output: " + out_audio_path)
+    ffmpeg.input(audio_path).output(out_audio_path).run()
+    return out_audio_path
+
+@views.route('/download_audio', methods=['GET'])
+def download_audio():
+    url = request.args.get('url')
+    if url:
+        # try:
+            video = YouTube(url)
+            audio = video.streams.get_audio_only()
+            audio_path = audio.download(download_directory)
+            out_audio_path = convert_to_mp3(audio_path)
+
+            if out_audio_path:
+                response = send_file(out_audio_path, as_attachment=True)
+                os.remove(audio_path)
+                os.remove(out_audio_path)
+                return response
+            else:
+                return "Audio conversion failed."
+        # except Exception as e:
+        #     return f"Error: {e}"
+
+    return "Invalid URL."
+        
